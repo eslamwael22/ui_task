315744642d70a3e92c2bd956b7b9ee01a6b3c30b
Eslam Wael <eldasheslam105@gmail.com>
2026-07-10 16:31:01 +0300

    order model added

---
+++ lib/features/Home/data/most_order_data.dart
@@ -0,0 +1,38 @@
+import '../models/most_order_model.dart';
+
+class MostOrderData {
+  static final List<MostOrderModel> items = [
+    MostOrderModel(
+      image: 'assets/images/Image 40.png',
+      title: 'كوك دور',
+      description: 'برجر • فرايد تشيكن',
+      rate: '4.5',
+      deliveryCost: '15 ج',
+      time: '45 دقيقة',
+    ),
+    MostOrderModel(
+      image: 'assets/images/spectra.png',
+      title: 'سبكترا كافيه',
+      description: 'فراخ • سندوتشات',
+      rate: '4.5',
+      deliveryCost: '20 ج',
+      time: '30 دقيقة',
+    ),
+    MostOrderModel(
+      image: 'assets/images/Image 40.png',
+      title: 'كوك دور',
+      description: 'برجر • فرايد تشيكن',
+      rate: '4.5',
+      deliveryCost: '15 ج',
+      time: '45 دقيقة',
+    ),
+    MostOrderModel(
+      image: 'assets/images/spectra.png',
+      title: 'سبكترا كافيه',
+      description: 'فراخ • سندوتشات',
+      rate: '4.5',
+      deliveryCost: '20 ج',
+      time: '30 دقيقة',
+    ),
+  ];
+}
